c812aebc6d7aa9bc0e54543ef33442328fe58829
Jonathan Campos <jonathan.campos.net@gmail.com>
2019-09-22 18:23:25 -0300

    Super classe para Enditades

--- demo-mvc/src/main/java/com/mballen/curso/boot/web/controller/CargoController.java
+++ demo-mvc/src/main/java/com/mballen/curso/boot/web/controller/CargoController.java
@@ -1,21 +1,21 @@
-package com.mballen.curso.boot.ewb.controller;
+package com.mballen.curso.boot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/funcionarios")
-public class FuncionarioController {
+@RequestMapping("/cargos")
+public class CargoController {
 	
 	@GetMapping("/cadastrar")
 	public String cadastrar() {
-		return ("/funcionario/cadastro");
+		return "/cargo/cadastro";
 	}
 	
-	@GetMapping("/listar")
+	@GetMapping("listar")
 	public String listar() {
-		return ("/funcionario/lista");
+		return "/cargo/lista";
 	}
 
 }
--- demo-mvc/src/main/java/com/mballen/curso/boot/web/controller/DepartamentoController.java
+++ demo-mvc/src/main/java/com/mballen/curso/boot/web/controller/DepartamentoController.java
@@ -1,4 +1,4 @@
-package com.mballen.curso.boot.ewb.controller;
+package com.mballen.curso.boot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
