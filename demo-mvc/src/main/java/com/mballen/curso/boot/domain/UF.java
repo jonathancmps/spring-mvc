73628946d734a87b19fd1d6f5eb46b8431f5e684
Jonathan Campos <jonathan.campos.net@gmail.com>
2019-09-29 17:06:33 -0300

    Criando um DAO Genérico

--- demo-mvc/src/main/java/com/mballen/curso/boot/domain/UF.java
+++ demo-mvc/src/main/java/com/mballen/curso/boot/domain/UF.java
@@ -40,16 +40,8 @@ public enum UF {
 	public String getSigla() {
 		return sigla;
 	}
-
-	public void setSigla(String sigla) {
-		this.sigla = sigla;
-	}
-
+	
 	public String getDescricao() {
 		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
 	}
 }
