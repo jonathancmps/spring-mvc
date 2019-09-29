704c4a7bc6c5dfa911140447d9d6be2551a717b7
Jonathan Campos <jonathan.campos.net@gmail.com>
2019-09-29 20:48:52 -0300

    Atualizando para o Spring-boot

--- demo-mvc/src/main/java/com/mballem/curso/boot/service/FuncionarioServiceImpl.java
+++ demo-mvc/src/main/java/com/mballem/curso/boot/service/FuncionarioServiceImpl.java
@@ -4,26 +4,31 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mballem.curso.boot.dao.FuncionarioDao;
 import com.mballen.curso.boot.domain.Funcionario;
 
 @Service
+@Transactional(readOnly=true)
 public class FuncionarioServiceImpl implements FuncionarioService {
 	
 	@Autowired
 	FuncionarioDao dao;
 
+	@Transactional(readOnly=false)
 	@Override
 	public void salvar(Funcionario funcionario) {
 		dao.save(funcionario);		
 	}
 
+	@Transactional(readOnly=false)
 	@Override
 	public void editar(Funcionario funcionario) {
 		dao.update(funcionario);		
 	}
 
+	@Transactional(readOnly=false)
 	@Override
 	public void excluir(Long id) {
 		dao.delete(id);		
